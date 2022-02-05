36d14bc540f66a3451dddf5dadd661401af6effd
Samuel Belete <samyethio891@gmail.com>
2022-02-05 11:04:24 -0800

    layouts created

--- router/admin.js
+++ router/admin.js
@@ -8,14 +8,14 @@ router.post("/add-product", (req, res, next) => {
     let prodName = req.body.title;
     let price = req.body.price;
     let desc = req.body.desc;
-    let iUrl = req.body.image;
-	data.push({ prodName, price, desc, iUrl });
+    // let iUrl = req.body.image;
+	data.push({ prodName, price, desc });
 	res.redirect("/");
 });
 
 router.get("/add-product", (req, res, next) => {
 	console.log(req.method);
-	res.render('./add-product.pug')
+	res.render("./add-product.pug", { docTitle:'Add Product'});
 });
 exports.router =router;
 exports.data = data;
